8556fc7692026da23a9316ceb4b77c5b0018112d
Shailendra pathak <pathakharsh813@gmail.com>
2025-05-08 14:38:18 +0530

    send email tool setup

---
+++ backend/mcp/server.mcp.js
@@ -0,0 +1,78 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { sendEmail } from "../src/service/google.service.js";
+import config from "../src/config/config.js";
+import mongoose from "mongoose";
+import {z} from "zod";
+import fs from "fs";
+
+
+mongoose.connect(config.MONGODB_URI)
+
+const server = new McpServer({
+  name: "example-server",
+  version: "1.0.0"
+});
+
+// ... set up server resources, tools, and prompts ...
+
+server.tool("addTwoNumbers", "add two numbers", {
+    a: z.number(),
+    b: z.number(),
+},
+async ({a, b}) => {
+     return {content: [{
+        type: "text",
+        text: `The sum of ${a} and ${b} is ${a + b}`
+     }]}
+} 
+)
+
+
+
+server.tool(
+    "sendEmail",
+    "send an email",
+    {
+        userId: z.string(),
+        to: z.string(),
+        subject: z.string(),
+        message: z.string()
+    },
+    async ({ userId, to, subject, message }) => {
+        try {
+
+            const result = await sendEmail(userId, to, subject, message);
+
+            return {
+                content: [ {
+                    type: "text",
+                    text: `Email sent to ${to} with subject "${subject}"`
+                } ]
+            }
+        } catch (error) {
+            console.error("Error sending email:", error);
+
+            fs.appendFileSync("./mcp.error.json", JSON.stringify({
+                userId,
+                to,
+                subject,
+                message,
+                error: error.message,
+            }));
+
+
+            return {
+                content: [ {
+                    type: "text",
+                    text: `Failed to send email to ${to} with subject "${subject}"`
+                } ]
+            }
+        }
+
+    }
+)
+
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -1,7 +1,11 @@
 import { GoogleGenAI } from "@google/genai";
 import config from '../config/config.js'
+import mcpClient from "../../mcp/client.mcp.js"
 
 const ai = new GoogleGenAI({ apiKey: config.GOOGLE_GEMINI_API_KEY });
+
+
+const tools = (await mcpClient.listTools()).tools
 
 async function getResponse(messages) {
     
@@ -9,7 +13,38 @@ async function getResponse(messages) {
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash-preview-04-17",
         contents: messages,
+        config: {
+            // systemInstruction: getSystemInstruction(user),
+            tools: [ {
+                functionDeclarations: tools.map(tool => {
+                    return {
+                        name: tool.name,
+                        description: tool.description,
+                        parameters: {
+                            type: tool.inputSchema.type,
+                            properties: tool.inputSchema.properties,
+                            required: tool.inputSchema.required,
+                        }
+                    }
+                })
+            } ]
+        }
     })
+
+    const functionCall = response.functionCalls && response.functionCalls[ 0 ]
+
+    if (functionCall) {
+
+        const toolResult = await mcpClient.callTool({
+            name: functionCall.name,
+            arguments: functionCall.args
+        })
+
+        const result = toolResult.content[ 0 ].text
+
+        return result
+
+    }
 
     const text = response.text
 
